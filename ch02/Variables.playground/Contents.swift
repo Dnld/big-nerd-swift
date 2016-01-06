a094a4f33db4dc813bad99f3ad09424ab8e9152e
Dnld <steinert.donald@gmail.com>
2016-01-06 11:24:49 -0500

    completes chapter 2

---
+++ ch02/Variables.playground/Contents.swift
@@ -0,0 +1,14 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+//var numberOfStopLights = "Four"
+//var numberOfStopLights: Int = 4
+//numberOfStopLights += 2
+let numberOfStopLights: Int = 4
+var population: Int
+population = 5422
+let townName: String = "Newtown"
+var unemploymentRate = "4.5"
+let townDescription = "\(townName) has a population of \(population) and \(numberOfStopLights) stoplights and a \(unemploymentRate)% unemployment rate."
+print(townDescription)
