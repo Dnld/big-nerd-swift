98ce56e617033c1597967e217e0af4dda2ebdb49
Dnld <steinert.donald@gmail.com>
2016-01-15 16:56:35 -0500

    init monster town

---
+++ ch15/MonsterTown/MonsterTown/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MonsterTown
+//
+//  Created by Donald Steinert on 1/15/16.
+//  Copyright © 2016 Donald Steinert. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
