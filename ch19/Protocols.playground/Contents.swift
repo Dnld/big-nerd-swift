1a44842790c37c6ec244278cb5253593a9d5f59d
Dnld <steinert.donald@gmail.com>
2016-01-25 14:35:31 -0500

    completes gold challenge

--- ch19/Protocols.playground/Contents.swift
+++ ch19/Protocols.playground/Contents.swift
@@ -95,7 +95,7 @@ protocol TabularDataSource {
     func labelForRow(row: Int) -> String
     func labelForColumn(column: Int) -> String
     
-    func itemForRow(row: Int, column: Int) -> Int
+    func itemForRow(row: Int, column: Int) -> String
 }
 
 struct Person {
@@ -140,11 +140,11 @@ struct Department: TabularDataSource, CustomStringConvertible {
         }
     }
     
-    func itemForRow(row: Int, column: Int) -> Int {
+    func itemForRow(row: Int, column: Int) -> String {
         let person = people[row]
         switch column {
-        case 0: return person.age
-        case 1: return person.yearsOfExperience
+        case 0: return String(person.age)
+        case 1: return String(person.yearsOfExperience)
         default: fatalError("Invalid column!")
         }
     }
@@ -157,3 +157,53 @@ department.addPerson(Person(name: "Karen", age: 40, yearsOfExperience: 18))
 department.addPerson(Person(name: "Fred", age: 50, yearsOfExperience: 20))
 
 printTable(department)
+
+struct Book {
+    let title: String
+    let author: String
+    let avgReviews: Int
+}
+
+struct BookCollection: TabularDataSource, CustomStringConvertible {
+    var books = [Book]()
+    var description: String {
+        return "This is a collection of books"
+    }
+    
+    var numberOfRows: Int {
+        return books.count
+    }
+    
+    var numberOfColumns: Int {
+        return 3
+    }
+    
+    func labelForRow(row: Int) -> String {
+        return String(row + 1)
+    }
+    
+    func labelForColumn(column: Int) -> String {
+        switch column {
+        case 0: return "Title"
+        case 1: return "Author"
+        case 2: return "Average Rating"
+        default: fatalError("Invalid column!")
+        }
+    }
+    
+    func itemForRow(row: Int, column: Int) -> String {
+        let book = books[row]
+        switch column {
+        case 0: return book.title
+        case 1: return book.author
+        case 2: return String(book.avgReviews)
+        default: fatalError("Invalid column!")
+        }
+    }
+    
+}
+
+var bookCollection = BookCollection()
+bookCollection.books.append(Book(title: "Infinite Jest", author: "David Foster Wallace", avgReviews: 5))
+bookCollection.books.append(Book(title: "The Right Stuff", author: "Tom Wolfe", avgReviews: 4))
+printTable(bookCollection)
