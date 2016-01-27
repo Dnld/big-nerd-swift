69095f95487ddea13075a72ce558d14a43003747
Dnld <steinert.donald@gmail.com>
2016-01-26 21:33:11 -0500

    init VocalTextEdit

---
+++ ch26/VocalTextEdit/VocalTextEdit/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  VocalTextEdit
+//
+//  Created by Donald Steinert on 1/26/16.
+//  Copyright © 2016 Donald Steinert. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
