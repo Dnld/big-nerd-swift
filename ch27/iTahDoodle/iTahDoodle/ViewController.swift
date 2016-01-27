b2aa489de2fe1cd61e737206715c59a44794fe9e
Dnld <steinert.donald@gmail.com>
2016-01-27 11:36:44 -0500

    initial functionality complete

--- ch27/iTahDoodle/iTahDoodle/ViewController.swift
+++ ch27/iTahDoodle/iTahDoodle/ViewController.swift
@@ -10,16 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var itemTextField: UITextField!
+    @IBOutlet var tableView: UITableView!
+    
+    let todoList = TodoList()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        tableView.dataSource = todoList
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func addButtonPressed(sender: UIButton) {
+        guard let text = itemTextField.text where text != "" else {
+            return
+        }
+        todoList.addItem(text)
+        tableView.reloadData()
+    }
 
 }
 
